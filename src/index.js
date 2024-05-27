fdff181c3be5cc71980d438061d6380ade08064b
YuyryPyshnov <coolspot64rus@gmail.com>
2024-05-27 21:48:41 +0400

    Подключил сервер API

--- src/index.js
+++ src/index.js
@@ -13,3 +13,8 @@ const changeActiveBtn = event => {
 buttons.forEach(button => {
 	button.addEventListener('click', changeActiveBtn)
 })
+
+const API_URL ='https://kind-frost-servant.glitch.me';
+// api/products/category/%D0%9B%D0%B5%D0%B6%D0%B0%D0%BD%D0%BA%D0%B8
+
+const productList = document.querySelector('.store__item');
